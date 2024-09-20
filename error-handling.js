80b45d81ce182f9bad4895c3740620ce569f6e98
Dancode-188 <danbitengo@gmail.com>
2024-09-20 21:08:24 +0300

    Practiced error handling

--- error-handling.js
+++ error-handling.js
@@ -1,10 +1,24 @@
 // 1.
 function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+  try {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return "Please provide an array"
+    }
   }
-  return sum;
+}
+
+function sayName(name) {
+  if (typeof name === "string") {
+    console.log(name);
+  } else {
+    throw new Error("Invalid name! Must be a string!");
+  }
 }
 
 let res = sum(null);
@@ -12,8 +26,17 @@ console.log(res);
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
+try {
+  sayName("Alex");
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  sayName(1);
+} catch (error) {
+  console.error(error.message);
+}
 // Your code here
 
 // 3.
@@ -23,4 +46,10 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try {
+  greet(null);
+} catch (error) {
+  console.error("Hello World!");
 }
